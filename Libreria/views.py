2e412d6f86a8ea1fd35c23c51fa587bea6e27e01
JDGM0331 <jesus.guevara0@misena.edu.co>
2022-09-13 14:54:41 -0500

    Views of the crud of books

--- Libreria/views.py
+++ Libreria/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+""" def inicio(request):
+    return HttpResponse("<h1>Bienvenido Develoteca</h1>") """
+
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido Develoteca</h1>")
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    return render(request, 'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')
