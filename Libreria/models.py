378f0e7ba948c0980dcce7c231c2101cecd597a1
JDGM0331 <jesus.guevara0@misena.edu.co>
2022-09-22 19:29:17 -0500

    Mostrar nombre de libro en admin, eliminar imagen almacenada en el proyecto

--- Libreria/models.py
+++ Libreria/models.py
@@ -6,3 +6,13 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='Título')
     imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
     descripcion = models.TextField(null=True, verbose_name='Descripción')
+
+    def __str__(self):
+        fila = "Título: "+self.titulo+" - "+"Descripción: "+self.descripcion
+        return fila
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+
+        
