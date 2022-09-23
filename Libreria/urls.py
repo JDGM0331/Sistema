d0b4b56d2751fe92f3181bad8ed9c78884139de8
JDGM0331 <jesus.guevara0@misena.edu.co>
2022-09-23 09:46:12 -0500

    Rutas para las imagenes y realizar registro

--- Libreria/urls.py
+++ Libreria/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from Libreria.views import nosotros
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('libros', views.libros, name='libros'),
     path('libros/crear', views.crear, name='crear'),
     path('libros/editar', views.editar, name='editar'),
-]   
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
--- Libreria/views.py
+++ Libreria/views.py
@@ -1,6 +1,9 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
+
 # Create your views here.
 
 """ def inicio(request):
@@ -13,10 +16,15 @@ def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def libros(request):
-    return render(request, 'libros/index.html')
+    libros = Libro.objects.all()
+    return render(request, 'libros/index.html', {'libros': libros})
 
 def crear(request):
-    return render(request, 'libros/crear.html')
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/crear.html', {'formulario':formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')
